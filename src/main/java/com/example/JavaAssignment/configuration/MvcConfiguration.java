6b3ad685837fb1b5711a390ac04aa35b05462e89
Amila1610 <amilahasovic@hotmail.com>
2022-03-24 15:26:24 +0100

    5.task

---
+++ src/main/java/com/example/JavaAssignment/configuration/MvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.JavaAssignment.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer { //5.task
+    
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/secure").setViewName("secure");
+    }
+}
